7dd67a424593839874b8d9c11318c30df77a4fc4
virus77z9x <virus77z9x@localhost.localdomain>
2025-07-23 02:12:18 +0300

    edit password login

--- app/Http/Controllers/Api/Vendor/Auth/AuthController.php
+++ app/Http/Controllers/Api/Vendor/Auth/AuthController.php
@@ -25,7 +25,7 @@ class AuthController extends Controller
         
         if (!Hash::check($request->password, $vendor->password)) {
             return response()->json([
-                'message' => __('Login fail'),
+                'message' => __('Login fail, Password not correct'),
             ], 401);
         }
         
