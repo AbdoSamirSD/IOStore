c526d135c7fc7695828fe741de1e18aebc51989b
virus77z9x <virus77z9x@localhost.localdomain>
2025-08-20 21:35:02 +0300

    cors error

--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie', 'vendor/*', 'admin/*'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'vendor/*', 'aapi/dmin/*'],
 
     'allowed_methods' => ['*'],
 
